26c422869547ad39e9bb8211bbe1857d7e3c96ee
Dasacav3 <dscv3719@gmail.com>
2021-04-07 13:15:42 -0500

    Actualización validación logout y login

--- app/models/loginUser.php
+++ app/models/loginUser.php
@@ -1,9 +1,14 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validando...</title>
     <!-- Sweer Alert -->
     <script src="../../lib/sweetaler2/sweetalert2.all.min.js" type="text/javascript"></script>
     <link rel="stylesheet" href="../../lib/sweetaler2/sweetalert2.min.css">
+    <link rel="shortcut icon" href="../views/dist/img/favicon.png">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
@@ -16,7 +21,6 @@
         }
     </style>
 </head>
-
 <body>
     <?php
 
