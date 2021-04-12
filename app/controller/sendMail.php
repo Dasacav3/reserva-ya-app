6877bb3eb07cb0783590bfb03df6e66db26585f3
Dasacav3 <dscv3719@gmail.com>
2021-04-12 09:57:35 -0500

    Correcion error sendEmail

--- app/controller/sendMail.php
+++ app/controller/sendMail.php
@@ -47,8 +47,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'micuenta3719@gmail.com';               //SMTP username
-    $mail->Password   = '&nuevacuentadegmail2019';              //SMTP password
+    $mail->Username   = '';               //SMTP username
+    $mail->Password   = '';              //SMTP password
     $mail->SMTPSecure = 'tls';                                  //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
