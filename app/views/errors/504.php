0dc3097c55ff64b5a5ef8761f6d7af155b33e875
Dasacav3 <dscv3719@gmail.com>
2021-04-17 21:17:28 -0500

    Actualización y preparación para estilizado de paginas errores http

---
+++ app/views/errors/504.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="../dist/img/favicon.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pagina no encontrada :c</title>
+</head>
+<body>
+    <h1>Pagina no encontrada</h1>
+    <div class="img-error">
+        <img src="cheems.jpg" alt="">
+    </div>
+    <p class="info-error">La pagina esta sufriendo de amsiedad, por favor vuelta a intentarlo</p>
+    <button class="button-return"><a href="http://localhost/reservaya-mvc/">Volver al inicio</a></button>
+</body>
+</html>
