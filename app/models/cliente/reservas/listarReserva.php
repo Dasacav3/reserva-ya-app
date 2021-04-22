2d75cdb168eac2dacbb57fb772b225eda798c8be
Dasacav3 <dscv3719@gmail.com>
2021-04-21 21:39:14 -0500

    Solución problema filtrado reservas cliente

--- app/models/cliente/reservas/listarReserva.php
+++ app/models/cliente/reservas/listarReserva.php
@@ -27,8 +27,7 @@ try {
         INNER JOIN reservacion ON reservacion_reserva_mesa.ID_RESERVACION = reservacion.ID_RESERVACION
         INNER JOIN mesa ON reservacion_reserva_mesa.ID_MESA = mesa.ID_MESA
         INNER JOIN cliente ON reservacion.ID_CLIENTE = cliente.ID_CLIENTE
-        WHERE cliente.ID_USUARIO = '$id'
-        WHERE reservacion.ESTADO_RESERVACION LIKE '%" . $data . "%' OR reservacion.ID_RESERVACION LIKE '%" . $data . "%' OR reservacion.FECHA_RESERVACION LIKE '%" . $data . "%' OR reservacion.HORA_RESERVACION LIKE '%" . $data . "%' OR mesa.ID_MESA LIKE '%" . $data . "%' OR reservacion.ASIENTO LIKE '%" . $data . "%'");
+        WHERE cliente.ID_USUARIO = '$id' AND (reservacion.ESTADO_RESERVACION LIKE '%" . $data . "%' OR reservacion.ID_RESERVACION LIKE '%" . $data . "%' OR reservacion.FECHA_RESERVACION LIKE '%" . $data . "%' OR reservacion.HORA_RESERVACION LIKE '%" . $data . "%' OR mesa.ID_MESA LIKE '%" . $data . "%' OR reservacion.ASIENTO LIKE '%" . $data . "%')");
         $query->execute();
     }
     $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
