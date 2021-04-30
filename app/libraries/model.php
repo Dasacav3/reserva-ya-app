65fe0578f742a3119d7bb00a76dc108a63aa24bf
Dasacav3 <dscv3719@gmail.com>
2021-04-30 17:15:47 -0500

    Preparando controladores para administración de session

---
+++ app/libraries/model.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once 'libraries/imodel.php';
+
+class Model{
+
+    function __construct(){
+        $this->db = new Database();
+    }
+
+    function query($query){
+        return $this->db->connect()->query($query);
+    }
+
+    function prepare($query){
+        return $this->db->connect()->prepare($query);
+    }
+}
