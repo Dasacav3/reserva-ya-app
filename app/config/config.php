402532a17b677f805f98a77f45c34f2a3a267b2d
Dasacav3 <dscv3719@gmail.com>
2021-04-29 22:06:06 -0500

    Añadiendo archivo config de constantes BD

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('URL', 'http://localhost/reservaya-mvc/');
+
+    define('HOST', 'localhost');
+    define('DB', 'reservaya2');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('CHARSET', 'utf8');
