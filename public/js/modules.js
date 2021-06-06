4cc418a3440256076e9535a3f64685e48dc832c0
Dasacav3 <dscv3719@gmail.com>
2021-06-06 16:48:29 -0500

    Cambio de ubicación actualización automatica de estado de reserva

--- public/js/modules.js
+++ public/js/modules.js
@@ -1,3 +1,14 @@
 "use strict";
 
 export const URL = "http://localhost/reservaya-mvc/";
+
+// Actualización automatica de estado de reserva
+export function updateStateReserva() {
+	fetch(URL + "app/controller/reservas/reservasEstado.php", {
+		method: "POST",
+	})
+		.then((res) => res.text())
+		.then((response) => {
+			console.log(response);
+		});
+}
