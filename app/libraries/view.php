c9305527e283ae9659bb4fb6fcce98099d9e5272
Dasacav3 <dscv3719@gmail.com>
2021-06-15 21:41:28 -0500

    Añadido metodos dashboard admin

--- app/libraries/view.php
+++ app/libraries/view.php
@@ -1,14 +1,16 @@
 <?php
 
+error_reporting(0);
+
 class View
 {
-
     private $session;
 
     public function __construct()
     {
         $this->mensaje = "";
         $this->session = new Session();
+        $this->model = new Model();
     }
 
     public function render($nombre)
@@ -19,5 +21,90 @@ class View
         } else {
             $path = new Error_Manage();
         }
+    }
+
+    public function getCantReservas()
+    {
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM reservacion");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
+    }
+
+    public function getCantInsumos(){
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM insumo");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
+    }
+
+    public function getCantProductos(){
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM producto");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
+    }
+
+    public function getCantProveedores(){
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM proveedor");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
+    }
+
+    public function getCantClientes(){
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM cliente");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
+    }
+
+    public function getCantEmpleados(){
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM empleado");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
+    }
+
+    public function getCantMesas(){
+        try {
+            $query = $this->model->prepare("SELECT COUNT(*) FROM mesa");
+            $query->execute();
+            $resultado = $query->fetch();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $resultado[0];
+        $this->model->close();
     }
 }
