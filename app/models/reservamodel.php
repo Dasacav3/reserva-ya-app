16d60234e8b0b62e34de1eb22932d2324715aae7
Dasacav3 <dscv3719@gmail.com>
2021-06-18 15:33:01 -0500

    Coreccion problema con reservas cliente

--- app/models/reservamodel.php
+++ app/models/reservamodel.php
@@ -109,6 +109,40 @@ class ReservaModel extends Model implements IModel
         $this->close();
     }
 
+    public function getAllCli($data)
+    {
+        try {
+            $query = $this->prepare("SELECT reservacion.ESTADO_RESERVACION, reservacion.ID_RESERVACION, cliente.NOMBRE_CLIENTE, cliente.APELLIDO_CLIENTE, 
+            reservacion.FECHA_RESERVACION, reservacion.HORA_RESERVACION, mesa.ID_MESA, reservacion.ASIENTO, reservacion_reserva_mesa.ID_RESERVACION_RESERVA_MESA
+            FROM reservacion_reserva_mesa
+            INNER JOIN reservacion ON reservacion_reserva_mesa.ID_RESERVACION = reservacion.ID_RESERVACION
+            INNER JOIN mesa ON reservacion_reserva_mesa.ID_MESA = mesa.ID_MESA
+            INNER JOIN cliente ON reservacion.ID_CLIENTE = cliente.ID_CLIENTE
+            WHERE reservacion.ESTADO_RESERVACION = 'Activa' AND cliente.ID_CLIENTE = :id
+            ORDER BY reservacion.FECHA_RESERVACION ASC");
+            $query->execute(['id' => $data['id']]);
+
+            if ($data['bus'] != "") {
+                $query = $this->prepare("SELECT reservacion.ESTADO_RESERVACION, reservacion.ID_RESERVACION, cliente.NOMBRE_CLIENTE, cliente.APELLIDO_CLIENTE, 
+                reservacion.FECHA_RESERVACION, reservacion.HORA_RESERVACION, mesa.ID_MESA, reservacion.ASIENTO,reservacion_reserva_mesa.ID_RESERVACION_RESERVA_MESA
+                FROM reservacion_reserva_mesa
+                INNER JOIN reservacion ON reservacion_reserva_mesa.ID_RESERVACION = reservacion.ID_RESERVACION
+                INNER JOIN mesa ON reservacion_reserva_mesa.ID_MESA = mesa.ID_MESA
+                INNER JOIN cliente ON reservacion.ID_CLIENTE = cliente.ID_CLIENTE
+                WHERE cliente.ID_CLIENTE = :id AND reservacion.ESTADO_RESERVACION = 'Activa' AND (reservacion.ESTADO_RESERVACION LIKE '%" . $data['bus'] . "%' OR reservacion.ID_RESERVACION LIKE '%" . $data['bus'] . "%' OR cliente.NOMBRE_CLIENTE LIKE '%" . $data['bus'] . "%' OR cliente.APELLIDO_CLIENTE LIKE '%" . $data['bus'] . "%' OR reservacion.FECHA_RESERVACION LIKE '%" . $data['bus'] . "%' OR reservacion.HORA_RESERVACION LIKE '%" . $data['bus'] . "%' OR mesa.ID_MESA LIKE '%" . $data['bus'] . "%' OR reservacion.ASIENTO LIKE '%" . $data['bus'] . "%')");
+                $query->execute(['id' => $data['id']]);
+            }
+            $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo "Conexion fallida " . $e->getMessage();
+            die();
+        }
+
+        echo json_encode($resultado);
+
+        $this->close();
+    }
+
     public function get($id)
     {
         try {
