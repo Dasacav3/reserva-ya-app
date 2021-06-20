0a1f9c21a1c1b6c06fe43074084bcd74656b70b0
Dasacav3 <dscv3719@gmail.com>
2021-06-19 21:14:50 -0500

    Actualizacion modelo reservacion

--- app/models/reservamodel.php
+++ app/models/reservamodel.php
@@ -95,7 +95,7 @@ class ReservaModel extends Model implements IModel
                 INNER JOIN reservacion ON reservacion_reserva_mesa.ID_RESERVACION = reservacion.ID_RESERVACION
                 INNER JOIN mesa ON reservacion_reserva_mesa.ID_MESA = mesa.ID_MESA
                 INNER JOIN cliente ON reservacion.ID_CLIENTE = cliente.ID_CLIENTE
-                WHERE reservacion.ESTADO_RESERVACION = 'Activa' AND (reservacion.ESTADO_RESERVACION LIKE '%" . $data . "%' OR reservacion.ID_RESERVACION LIKE '%" . $data. "%' OR cliente.NOMBRE_CLIENTE LIKE '%" . $data . "%' OR cliente.APELLIDO_CLIENTE LIKE '%" . $data . "%' OR reservacion.FECHA_RESERVACION LIKE '%" . $data['bus'] . "%' OR reservacion.HORA_RESERVACION LIKE '%" . $data . "%' OR mesa.ID_MESA LIKE '%" . $data . "%' OR reservacion.ASIENTO LIKE '%" . $data['bus'] . "%')");
+                WHERE reservacion.ESTADO_RESERVACION = 'Activa' AND (reservacion.ESTADO_RESERVACION LIKE '%" . $data . "%' OR reservacion.ID_RESERVACION LIKE '%" . $data. "%' OR cliente.NOMBRE_CLIENTE LIKE '%" . $data . "%' OR cliente.APELLIDO_CLIENTE LIKE '%" . $data . "%' OR reservacion.FECHA_RESERVACION LIKE '%" . $data . "%' OR reservacion.HORA_RESERVACION LIKE '%" . $data . "%' OR mesa.ID_MESA LIKE '%" . $data . "%' OR reservacion.ASIENTO LIKE '%" . $data . "%')");
                 $query->execute();
             }
             $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
