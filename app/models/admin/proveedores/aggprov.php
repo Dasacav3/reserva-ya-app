aee98b1d4a207a64c383fac4e0d1de9bd6a6ddab
Dasacav3 <dscv3719@gmail.com>
2021-09-13 08:32:11 -0500

    Actualización urls modulo proveedor

--- app/models/admin/proveedores/aggprov.php
+++ app/models/admin/proveedores/aggprov.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>php</title>
+	<title>Agregar proveedor</title>
 </head>
+
 <body>
 	<?php
 	include('../../../controller/database.php');
-	try{
-		$query=$pdo->prepare("INSERT INTO PROVEEDOR (NOMBRE_PROVEEDOR,DIRECCION_PROVEEDOR,PERSONA_ENCARGADA,TELEFONO_PROVEEDOR) VALUES (:nombre,:direccion,:persona,:cel)");
-		$query->bindParam(":nombre",$_REQUEST['NOMBRE_PROVEEDOR']);
-		$query->bindParam(":direccion",$_REQUEST['DIRECCION_PROVEEDOR']);
-		$query->bindParam(":persona",$_REQUEST['PERSONA_ENCARGADA']);
-		$query->bindParam(":cel",$_REQUEST['TELEFONO_PROVEEDOR']);
+	try {
+		$query = $pdo->prepare("INSERT INTO proveedor (NOMBRE_PROVEEDOR,DIRECCION_PROVEEDOR,PERSONA_ENCARGADA,TELEFONO_PROVEEDOR) VALUES (:nombre,:direccion,:persona,:cel)");
+		$query->bindParam(":nombre", $_REQUEST['NOMBRE_PROVEEDOR']);
+		$query->bindParam(":direccion", $_REQUEST['DIRECCION_PROVEEDOR']);
+		$query->bindParam(":persona", $_REQUEST['PERSONA_ENCARGADA']);
+		$query->bindParam(":cel", $_REQUEST['TELEFONO_PROVEEDOR']);
 		$query->execute();
-	}catch(Exception $e){
+	} catch (Exception $e) {
 		echo "Conexion Fallida: " . $e->getMessage();
-	}	
- 	echo "<script>alert('Se agrego correctamente');</script>";
+	}
+	echo "<script>alert('Se agrego correctamente');</script>";
 
-?>
+	?>
 
 	<script>
-         top.location.href="<?= constant('URL') ?>admin/proveedores";
-      </script>
+		top.location.href = "<?= constant('URL') ?>admin/proveedores";
+	</script>
 
-<?php
-	$pdo=null;
-?>
+	<?php
+	$pdo = null;
+	?>
 </body>
+
 </html>
