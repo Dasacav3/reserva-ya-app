ca9166f7ae9fc6429db79687206242837f93d978
Dasacav3 <dscv3719@gmail.com>
2021-09-24 06:38:00 -0500

    Actualización alerta eliminar producto

--- app/views/admin/productos.php
+++ app/views/admin/productos.php
@@ -214,31 +214,6 @@
 <script src="<?= constant('URL') ?>public/js/app.js" type="module"></script>
 <script src="<?= constant('URL') ?>public/js/sidebarDashboard.js" type="module"></script>
 <script src="<?= constant('URL') ?>public/js/alert_product.js"></script>
-<script>
-  window.addEventListener('DOMContentLoaded', () => {
-    $(".obtener_eliminar").on("click", function(e) {
-      e.preventDefault();
-      const href = $(this).attr("href");
-      Swal.fire({
-        title: "¿Deseas Eliminar?",
-        text: "Esta acción no se puede revertir",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Aceptar",
-        cancelButtonText: "Cancelar",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire("Eliminado", "El producto ha sido eliminado", "success");
-        }
-        if (result.value) {
-          document.location.href = href;
-        }
-      });
-    });
-  })
-</script>
 <script src="<?= constant('URL') ?>public/js/modales_productos.js"></script>
 
 </html>
--- public/js/modales_productos.js
+++ public/js/modales_productos.js
@@ -75,6 +75,28 @@ window.addEventListener("DOMContentLoaded", () => {
 		for (let i = 0; i < btnEdit.length; i++) {
 			btnEdit[i].addEventListener("click", llenarmodal, false);
 		}
+
+		$(".obtener_eliminar").on("click", function (e) {
+			e.preventDefault();
+			const href = $(this).attr("href");
+			Swal.fire({
+				title: "¿Deseas Eliminar?",
+				text: "Esta acción no se puede revertir",
+				icon: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#3085d6",
+				cancelButtonColor: "#d33",
+				confirmButtonText: "Aceptar",
+				cancelButtonText: "Cancelar",
+			}).then((result) => {
+				if (result.isConfirmed) {
+					Swal.fire("Eliminado", "El producto ha sido eliminado", "success");
+				}
+				if (result.value) {
+					document.location.href = href;
+				}
+			});
+		});
 	}
 
 	// Traer datos ventana modal editar
