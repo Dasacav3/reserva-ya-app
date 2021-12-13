3767957613bd2e760c8b0f6fcfd845308796d78b
Dasacav3 <dscv3719@gmail.com>
2021-12-13 18:29:52 -0500

    Updated database configuration

--- app/controller/database.php
+++ app/controller/database.php
@@ -1,9 +1,11 @@
 <?php
 // Configuracion de la BD
 
-$servidor = "mysql:dbname=reservaya;host=localhost";
-$user = "root";
-$pass = "mariadb";
+include __DIR__ . '/../config/config.php';
+
+$servidor = "mysql:host=".constant('HOST').";dbname=".constant('DB').";port=".constant('PORT');
+$user = constant('USER');
+$pass = constant('PASSWORD');
 try {
     $pdo = new PDO($servidor, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 } catch (PDOException $e) {
--- app/libraries/database.php
+++ app/libraries/database.php
@@ -8,6 +8,7 @@ class Database
     private $user;
     private $password;
     private $charset;
+    private $port;
 
     public function __construct()
     {
@@ -16,12 +17,13 @@ class Database
         $this->user = constant('USER');
         $this->password = constant('PASSWORD');
         $this->charset = constant('CHARSET');
+        $this->port = constant('PORT');
     }
 
     protected function connect()
     {
         try {
-            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";port=" . $this->port .  ";charset=" . $this->charset;
             $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false];
             $pdo = new PDO($connection, $this->user, $this->password, $options);
 
